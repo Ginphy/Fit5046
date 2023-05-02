4811dcfa28d971fb6e282c592d414001495821ac
Huang Yuhang <HuangYH723@outlook.com>
2023-05-02 21:21:56 +0800

    登录功能实现

--- app/src/main/java/com/example/ce/ui/login/SignupActivity.java
+++ app/src/main/java/com/example/ce/ui/login/SignupActivity.java
@@ -3,18 +3,24 @@ package com.example.ce.ui.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ce.MainActivity;
 import com.example.ce.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity  extends AppCompatActivity {
     private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
@@ -26,14 +32,38 @@ public class SignupActivity  extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email_txt = emailEditText.getText().toString();
+                String password_txt = passwordEditText.getText().toString();
+                if (TextUtils.isEmpty(email_txt) ||
+                        TextUtils.isEmpty(password_txt)) {
+                    String msg = "Empty Username or Password";
+                } else if (password_txt.length() < 6) {
+                    String msg = "Password is too short";
+                } else
+                    registerUser(email_txt, password_txt);
                 finish();
                 Intent i = new Intent(SignupActivity.this, MainActivity.class);
                 startActivity(i);
             }
         });
+    }
 
+    private void registerUser(String email_txt, String password_txt) {
+        // To create username and password
+        auth.createUserWithEmailAndPassword(email_txt, password_txt).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    String msg = "Registration Successful";
+                } else {
+                    String msg = "Registration Unsuccessful";
+                    toastMsg(msg);
+                }
+            }
+        });
+    }
 
-
-
+    public void toastMsg(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
