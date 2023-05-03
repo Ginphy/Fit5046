2e3005c6d1e9f11a17803a8f5285eb9f7d8f2af4
virgoghost <jzha0424@student.monash.edu>
2023-05-03 17:36:27 +0800

    added infoActivity logic

--- app/src/main/java/com/example/ce/ui/login/InfoActivity.java
+++ app/src/main/java/com/example/ce/ui/login/InfoActivity.java
@@ -2,41 +2,65 @@ package com.example.ce.ui.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ce.R;
 
 public class InfoActivity extends AppCompatActivity {
-
+    RadioGroup rg = (RadioGroup) findViewById(R.id.rg_sex);;
+    RadioButton rb_Male = (RadioButton) findViewById(R.id.male);;
+    RadioButton rb_Female = (RadioButton) findViewById(R.id.female);
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
-        setContentView(R.layout.start);
+        setContentView(R.layout.info);
 
 
-        Button btnSignup = findViewById(R.id.Sign);
+
+        Button btnBack = findViewById(R.id.back);
         Button btnLogin = findViewById(R.id.Log);
-
+        rg.setOnCheckedChangeListener(new RadioButtonListener());
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
-                Intent i = new Intent(StartActivity.this, LoginActivity.class);
+                Intent i = new Intent(InfoActivity.this, LoginActivity.class);
                 startActivity(i);
             }
         });
 
-        btnSignup.setOnClickListener(new View.OnClickListener(){
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View view) {
                 finish();
-                Intent i = new Intent(StartActivity.this, SignupActivity.class);
+                Intent i = new Intent(InfoActivity.this, SignupActivity.class  );
                 startActivity(i);
+
             }
         });
+    }
+    public class RadioButtonListener implements RadioGroup.OnCheckedChangeListener {
+
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int i) {
+            switch (i){
+                case R.id.male:
+                    //user choose user option
+                    Log.i("Role","Current user select" + rb_Male.getText().toString());
+                    break;
+                case R.id.female:
+                    //user choose courier option
+                    Log.i("Role", "Current user select"+ rb_Female.getText().toString());
+                    break;
+            }
+
+        }
     }
 
 }
