0acbc3e6042eb871ae51a72bcc412cbf27ebd3cf
Huang Yuhang <HuangYH723@outlook.com>
2023-05-04 15:43:49 +0800

    个人信息展示完成√

--- app/src/main/java/com/example/ce/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/ce/ui/notifications/NotificationsFragment.java
@@ -1,6 +1,7 @@
 package com.example.ce.ui.notifications;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,27 +9,45 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.ce.R;
 import com.example.ce.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         NotificationsViewModel notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
-
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        TextView Profile = root.findViewById(R.id.Profile);
+        TextView NickName = root.findViewById(R.id.Nickname);
+        TextView Name = root.findViewById(R.id.Name);
+        TextView Sex = root.findViewById(R.id.Gender);
+        TextView Identity = root.findViewById(R.id.Identity);
+        TextView Phone = root.findViewById(R.id.phone);
+        TextView Email = root.findViewById(R.id.Email);
+        notificationsViewModel.getData().observe(getViewLifecycleOwner(), new Observer<NotificationsViewModel.PersonalInfo>() {
+            @Override
+            public void onChanged(NotificationsViewModel.PersonalInfo personalInfo) {
+                Profile.setText(String.format("Profile: %s", personalInfo.Profile));
+                NickName.setText(String.format("Nickname: %s", personalInfo.Nickname));
+                Name.setText(String.format("Name: %s", personalInfo.Name));
+                Identity.setText(String.format("IdentityNum: %s", personalInfo.Identity));
+                Sex.setText(String.format("Gender: %s", personalInfo.Gender));
+                Phone.setText(String.format("Nickname: %s", personalInfo.Phone));
+                Email.setText(String.format("E-mail: %s", personalInfo.Email));
 
-//        final TextView textView = binding.textNotifications;
-//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+            }
+        });
         return root;
     }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
--- app/src/main/java/com/example/ce/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/example/ce/ui/notifications/NotificationsViewModel.java
@@ -1,19 +1,97 @@
 package com.example.ce.ui.notifications;
 
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.ce.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class NotificationsViewModel extends ViewModel {
+    private final MutableLiveData<PersonalInfo> mData;
+    private PersonalInfo mOldData;
+    public void Getprofile(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth auth;
+        auth = FirebaseAuth.getInstance();
+        String id = auth.getCurrentUser().getUid();
+        Log.d(TAG, "id: "+id);
+        // 获取users集合中ID的文档
+        // mOldData.Profile = personaldata[0];
+        //        mOldData.Name = personaldata[1];
+        //        mOldData.Nickname = "Click to set";
+        //        mOldData.Gender = personaldata[2];
+        //        mOldData.Identity = personaldata[3];
+        //        mOldData.Phone = personaldata[4];
+        //        mOldData.Email = personaldata[5];
+        //
+        //        mData.postValue(mOldData);
+        db.collection("users")
+                .whereEqualTo("uid", id)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                mOldData.Profile = document.getString("uid");
+                                mOldData.Nickname = "Click to Set!";
+                                mOldData.Name = document.getString("name");
+                                mOldData.Gender = document.getString("sex");
+                                mOldData.Identity = document.getString("identity");
+                                mOldData.Phone = document.getString("phone");
+                                mOldData.Email = document.getString("email");
+                                mOldData.Credit = document.getLong("credit").intValue();
 
-    private final MutableLiveData<String> mText;
-
+                                Log.d(TAG, "Successful getting documents!" + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                        mData.postValue(mOldData);
+                    }
+                });
+    }
     public NotificationsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mOldData = new PersonalInfo();
+        mOldData.Profile = "Profile";
+        mOldData.Email = "E-mail";
+        mOldData.Gender = "TBD";
+        mOldData.Identity = "110";
+        mOldData.Name = "ZJJSB";
+        mOldData.Nickname = "I am Pig";
+        mOldData.Phone = "110";
+        mOldData.Credit = 100;
+
+
+        mData = new MutableLiveData<>();
+        mData.setValue(mOldData);
+    }
+    public LiveData<PersonalInfo> getData() {
+        Getprofile();
+        return mData;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+
+    public class PersonalInfo {
+        public String Profile;
+        public String Nickname;
+        public String Name;
+        public String Gender;
+        public String Identity;
+        public String Phone;
+        public String Email;
+        public int Credit;
     }
 }
