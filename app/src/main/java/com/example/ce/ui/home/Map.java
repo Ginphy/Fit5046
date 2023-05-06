9ce79c01369873358308cbbd3123312a0d1a18ce
Huang Yuhang <HuangYH723@outlook.com>
2023-05-06 21:50:13 +0800

    Map能提示，但MD5码未通过

--- app/src/main/java/com/example/ce/ui/home/Map.java
+++ app/src/main/java/com/example/ce/ui/home/Map.java
@@ -15,17 +15,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.amap.api.maps.AMap;
-import com.amap.api.services.core.ServiceSettings;
-
-
+import com.amap.api.maps.CameraUpdate;
+import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.MyLocationStyle;
+
+import com.amap.api.maps.model.MyLocationStyle;
 import com.amap.api.services.core.ServiceSettings;
 import com.example.ce.R;
 import com.example.ce.ui.login.InfoActivity;
 import com.example.ce.ui.login.LoginActivity;
 import com.example.ce.ui.login.StartActivity;
 
-
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -68,39 +70,27 @@ public class Map extends Activity {
             if (aMap == null) {
                 aMap = mMapView.getMap();
             }
+            MyLocationStyle myLocationStyle;
+            myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+            myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+            aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+            aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+            aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false
+            aMap.moveCamera(CameraUpdateFactory.zoomTo(20));//地图显示级别
         } catch (Exception e) {
             System.out.print("e=" + e);
         }
 
-        SearchStart.setOnClickListener(new View.OnClickListener(){
+        SearchStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent i = new Intent(Map.this, SearchActivity.class);
-                // startActivity(i);
+            public void onClick(View view) {
+                startActivity(new Intent(Map.this, SearchActivity.class));
             }
         });
-
-
-
-
-
-
         // setInit();
         // initMap();
     }
 
-
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
 
     @Override
     protected void onResume() {
@@ -117,6 +107,14 @@ public class Map extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        //在activity executes onDestroy() mMapView.onDestroy(),Destroy Map
         mMapView.onDestroy();
     }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+
 }
--- app/src/main/java/com/example/ce/ui/home/SearchActivity.java
+++ app/src/main/java/com/example/ce/ui/home/SearchActivity.java
@@ -1,4 +1,93 @@
 package com.example.ce.ui.home;
 
-public class SearchActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Poi;
+//import com.amap.api.navi.AMapNavi;
+//import com.amap.api.navi.AmapNaviPage;
+//import com.amap.api.navi.AmapNaviParams;
+//import com.amap.api.navi.AmapNaviType;
+//import com.amap.api.navi.AmapPageType;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.help.Inputtips;
+import com.amap.api.services.help.InputtipsQuery;
+import com.amap.api.services.help.Tip;
+import com.example.ce.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity implements Inputtips.InputtipsListener, TextWatcher, RvAdapter.OnItemClickListener {
+    private RvAdapter rvAdapter;
+    private Inputtips inputTips;
+//    private AMapNavi mAMapNavi;
+    private EditText editText;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchpoi);
+        EditText editText = findViewById(R.id.edit_query);
+        editText.addTextChangedListener(this);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        rvAdapter = new RvAdapter(this, recyclerView, new ArrayList<>());
+        rvAdapter.setOnItemClickListener(this);
+        recyclerView.setAdapter(rvAdapter);
+
+        //构造 Inputtips 对象，并设置监听
+        inputTips = new Inputtips(this, (InputtipsQuery) null);
+        //指定setInputtipsListener得到onGetInputtips结果的回调
+        inputTips.setInputtipsListener(this);
+
+
+    }
+
+    //通过适配器把数据展示到recyclerView中去
+    @Override
+    public void onGetInputtips(List<Tip> list, int i) {
+        rvAdapter.setData(list);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        // 构造InputtipsQuery 对象，通过 InputtipsQuery(第一个参数为输入的数据，第二个参数城市) 设置搜索条件，null为全国范围。
+        InputtipsQuery inputquery = new InputtipsQuery(String.valueOf(s), null);
+        inputquery.setCityLimit(true);//限制在当前城市
+        inputTips.setQuery(inputquery);
+        //异步提示请求
+        inputTips.requestInputtipsAsyn();
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+    public void onItemClick(RecyclerView parent, View view, int position, Tip data) {
+        //得到点击的坐标
+        LatLonPoint point = data.getPoint();
+        //得到经纬度
+        Poi poi = new Poi(data.getName(), new LatLng(point.getLatitude(), point.getLongitude()), data.getPoiID());
+//        //导航参数对象（起点，途径，终点，导航方式）DRIVER是导航方式（驾驶，步行...当前为驾驶）ROUTE会计算路程选择
+//        AmapNaviParams params = new AmapNaviParams(null, null, poi, AmapNaviType.DRIVER, AmapPageType.ROUTE);
+//        //传递上下文和导航参数
+//        AmapNaviPage.getInstance().showRouteActivity(getApplicationContext(), params, null);
+    }
 }
