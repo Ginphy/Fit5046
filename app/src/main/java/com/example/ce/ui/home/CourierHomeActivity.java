5ece1ad238b8088e701c2ffa0f60f4ed763d5043
Jokery-max <115686545+Jokery-max@users.noreply.github.com>
2023-05-09 00:01:55 +0800

    顶栏和优化

--- app/src/main/java/com/example/ce/ui/home/CourierHomeActivity.java
+++ app/src/main/java/com/example/ce/ui/home/CourierHomeActivity.java
@@ -1,7 +1,17 @@
 package com.example.ce.ui.home;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.ce.R;
 
 public class CourierHomeActivity extends Activity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.courier_home);
+    }
 }
